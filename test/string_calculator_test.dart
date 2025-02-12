b6c2f3d94e98ef8ffbadfaeb36980816e56807d5
Hemant <hemantbansal172000@gmail.com>
2025-02-12 11:26:06 +0530

    Empty string returns zero

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -1,8 +1,14 @@
-import 'package:test/test.dart';
 import 'package:string_calculator/string_calculator.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  late StringCalculator calculator;
+
+  setUp(() {
+    calculator = StringCalculator();
+  });
+
+  test('empty string returns zero', () {
+    expect(calculator.add(""), equals(0));
   });
 }
