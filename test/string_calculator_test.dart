a649df71e8a728f93504142f3198ead78ed292fe
Hemant <hemantbansal172000@gmail.com>
2025-02-12 11:29:31 +0530

    Add support for single number input

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -11,4 +11,10 @@ void main() {
   test('empty string returns zero', () {
     expect(calculator.add(""), equals(0));
   });
+
+  test('single number returns that number', () {
+    expect(calculator.add("1"), equals(1));
+    expect(calculator.add("5"), equals(5));
+    expect(calculator.add("42"), equals(42));
+  });
 }
